32c7a6473d46a1c63a5d72a474f0186560a562f6
Eugene <doodleoocrypto@gmail.com>
2024-11-18 17:19:41 +0300

    Update cron.ts

--- src/cron.ts
+++ src/cron.ts
@@ -1,17 +1,21 @@
 import { kv } from "app/services/storage.ts";
 import { bridgeNodesAPI } from "app/services/api.ts";
 import { disApi } from "app/utils.ts";
-// Deno.cron("Check bridge nodes", "*/5 * * * *", async () => {
+import { EmbedBuilder } from "discord.js";
+
+Deno.cron("Check bridge nodes", "*/5 * * * *", async () => {
   const CONNECTED_PEERS_THRESHOLD = 5;
   const LAST_SYNC_THRESHOLD = 5;
   const nodesIds = await bridgeNodesAPI.getAllBridgeNodesIds();
   const nodeCache = new Map();
+
   for (const nodeId of nodesIds) {
     const buildInfo = await bridgeNodesAPI.buildInfo(nodeId);
     const connectedPeers = await bridgeNodesAPI.connectedPeers(nodeId);
     const syncStatus = await bridgeNodesAPI.syncStatus(nodeId);
     nodeCache.set(nodeId, { buildInfo, connectedPeers, syncStatus });
   }
+
   const subs = kv.list({
     prefix: ["subscription"],
   });
@@ -19,28 +23,37 @@ import { disApi } from "app/utils.ts";
   for await (const { key, value: _value } of subs) {
     const [_, userId, nodeId] = key;
     const nodeData = nodeCache.get(nodeId);
-    if (!nodeData) {
-      continue;
-    }
+    if (!nodeData) continue;
+
     const { connectedPeers, syncStatus } = nodeData;
     const grabLastSyncStatus = syncStatus.reverse().slice(-LAST_SYNC_THRESHOLD);
     const sumSync = grabLastSyncStatus.reduce(
-      (acc: number, curr: { value: number }) => acc + curr.value,
-      0,
+        (acc: number, curr: { value: number }) => acc + curr.value,
+        0,
     );
+
     if (sumSync >= LAST_SYNC_THRESHOLD) {
-      disApi.sendMessageBotChannel(
-        `Hey <@${String(userId)}>\n seems node \`${
-          String(nodeId)
-        }\` is not syncing, please pay attention`,
-      );
+      const embed = new EmbedBuilder()
+          .setTitle("Warning! Node sync alert")
+          .setDescription(`<@${String(userId)}>, your node **${String(nodeId)}** is out of sync and not syncing right now`)
+          .setColor(0xf3cd37)
+          .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+          .setFooter({ text: "Made by www.dteam.tech \uD83D\uDFE0" })
+          .setTimestamp(new Date())
+
+      disApi.sendEmbedMessageBotChannel(embed);
     }
-    if (connectedPeers.value.value < CONNECTED_PEERS_THRESHOLD) {
-      disApi.sendMessageBotChannel(
-        `Hey <@${String(userId)}>\nNode: \`${
-          String(nodeId)
-        }\` has less than ${CONNECTED_PEERS_THRESHOLD} connected peers`,
-      );
+
+    if (connectedPeers.value.value < CONNECTED_PEERS_THRESHOLD && sumSync < LAST_SYNC_THRESHOLD) {
+      const embed = new EmbedBuilder()
+          .setTitle("Warning! Low peer count alert")
+          .setDescription(`<@${String(userId)}>, your node **${String(nodeId)}** has less than ${CONNECTED_PEERS_THRESHOLD} connected peers`)
+          .setColor(0xf3cd37)
+          .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+          .setFooter({ text: "Made by www.dteam.tech \uD83D\uDFE0" })
+          .setTimestamp(new Date())
+
+      disApi.sendEmbedMessageBotChannel(embed);
     }
   }
-// });
+});
