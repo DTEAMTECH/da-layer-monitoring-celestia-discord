a79300c9e2e820c43f43b49b0f0a3124538767e7
ruslanglaznyov <ruslan.glaznyov@gmail.com>
2024-11-21 19:36:59 +0300

    feat: alerts

---
+++ src/constant.ts
@@ -0,0 +1,6 @@
+export const CONNECTED_PEERS_THRESHOLD = 2;
+// Should be more then Promethus scrape interval in 2 times
+export const SYNC_TIME_CHECK = "10m";
+export const OUT_OF_SYNC_HEIGHT_THRESHOLD = 10;
+// The time in minutes to consider an alert recent, to avoid spamming the user alerts 
+export const TIME_RECENT_ALERT_IN_MINUTES = 120;
