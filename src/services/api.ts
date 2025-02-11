475b415bdb23ccae1a44af00c78c0f8b2dfbaec8
Eugene <doodleoocrypto@gmail.com>
2025-02-11 18:45:55 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import { InstantVector, PrometheusDriver } from "prometheus-query";
 import config from "app/config.ts";
 
-class BridgeNodesAPI {
+class NodesAPI {
   public promQuery: PrometheusDriver;
+
   constructor(public promURL: string) {
     this.promURL = promURL;
     this.promQuery = new PrometheusDriver({
@@ -10,27 +11,55 @@ class BridgeNodesAPI {
       baseURL: "/api/v1",
     });
   }
-  public async getAllBridgeNodesIds() {
+
+  public async getAllNodesIds(): Promise<string[]> {
     const date = new Date();
     date.setDate(date.getDate() - 1);
     const data = await this.promQuery.labelValues(
-      "exported_instance",
-      undefined,
-      date,
-      new Date(),
+        "exported_instance",
+        undefined,
+        date,
+        new Date()
     );
     const ipRegex = /\b(?:\d{1,3}\.){3}\d{1,3}\b/;
     return data.filter((nodeId) => !ipRegex.test(nodeId));
   }
+
   public async buildInfo(nodeId: string): Promise<InstantVector | null> {
     const data = await this.promQuery.instantQuery(
-      `build_info{exported_instance="${nodeId}", exported_job="celestia/Bridge" }`,
+        `build_info{exported_instance="${nodeId}"}`
     );
-    const [result] = data.result;
-    if (!result) {
+    if (!data.result || data.result.length === 0) {
       return null;
     }
-    return result;
+    return data.result[0];
+  }
+
+  public async getNodeType(nodeId: string): Promise<string | null> {
+    try {
+      const data = await this.promQuery.instantQuery(
+          `build_info{exported_instance="${nodeId}"}`
+      );
+      if (!data.result || data.result.length === 0) {
+        return null;
+      }
+
+      for (const result of data.result) {
+        const jobLabel: string | undefined = result.metric.labels.exported_job;
+
+        if (jobLabel && jobLabel.startsWith("celestia/")) {
+          const parts = jobLabel.split("/");
+          if (parts.length >= 2) {
+            return parts[1];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      console.error(`Error while getting type of node ${nodeId}:`, error);
+      return null;
+    }
   }
 }
-export const bridgeNodesAPI = new BridgeNodesAPI(config.PROMETHEUS_URL);
+
+export const nodesAPI = new NodesAPI(config.PROMETHEUS_URL);
