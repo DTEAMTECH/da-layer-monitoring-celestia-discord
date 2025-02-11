377dd2cac4e16c3dfe535473c3c900bfcc556865
Eugene <doodleoocrypto@gmail.com>
2025-02-11 18:45:49 +0300

    Update subscribe.ts

--- src/cmds/general/subscribe.ts
+++ src/cmds/general/subscribe.ts
@@ -2,53 +2,55 @@ import {
   EmbedBuilder,
   SlashCommandBuilder,
   SlashCommandStringOption,
+  ApplicationCommandOptionType,
 } from "discord.js";
 import { kv } from "app/services/storage.ts";
-import { bridgeNodesAPI } from "app/services/api.ts";
-
+import { nodesAPI } from "app/services/api.ts";
 import type { Command } from "app/cmds/mod.ts";
 import { json } from "sift/mod.ts";
-import {
+import type {
   APIApplicationCommandAutocompleteInteraction,
-  ApplicationCommandOptionType,
 } from "discord.js";
 
 const command = new SlashCommandBuilder()
-  .setName("subscribe")
-  .setDescription("Subscribe for the updates about your bridge node")
-  .addStringOption((option: SlashCommandStringOption) =>
-    option.setName("id")
-      .setDescription("Bridge node id")
-      .setRequired(true)
-      .setAutocomplete(true)
-  );
+    .setName("subscribe")
+    .setDescription("Subscribe for the updates about your node")
+    .addStringOption((option: SlashCommandStringOption) =>
+        option
+            .setName("id")
+            .setDescription("Node id (e.g. 12D3Koo... (Bridge))")
+            .setRequired(true)
+            .setAutocomplete(true)
+    );
 
 const autocomplete = async (
-  interaction: APIApplicationCommandAutocompleteInteraction,
+    interaction: APIApplicationCommandAutocompleteInteraction,
 ) => {
-  const nodesIds = await bridgeNodesAPI.getAllBridgeNodesIds();
-  const choices = nodesIds.map((nodeId) => ({
-    name: nodeId,
-    value: nodeId,
-  }));
-
+  const nodesIds = await nodesAPI.getAllNodesIds();
+  const choicesPromises = nodesIds.map(async (nodeId) => {
+    const nodeType = await nodesAPI.getNodeType(nodeId);
+    return {
+      name: `${nodeId} (${nodeType ?? "Unknown"})`,
+      value: nodeId,
+    };
+  });
+  const choices = await Promise.all(choicesPromises);
   const findData = interaction.data.options.find((opt) => opt.name === "id");
-
   if (
-    findData && findData?.type === ApplicationCommandOptionType.String &&
-    findData.value.length
+      findData &&
+      findData.type === ApplicationCommandOptionType.String &&
+      findData.value.length
   ) {
-    const filtredChoices = choices.filter((choice) =>
-      choice.name.includes(findData.value)
+    const filteredChoices = choices.filter((choice) =>
+        choice.name.includes(findData.value)
     );
     return json({
       type: 8,
       data: {
-        choices: filtredChoices.slice(0, 5),
+        choices: filteredChoices.slice(0, 5),
       },
     });
   }
-
   return json({
     type: 8,
     data: {
@@ -63,19 +65,12 @@ export const subscribe: Command = {
   execute: async (data, interaction) => {
     if (!interaction.member) {
       const embed = new EmbedBuilder()
-        .setTitle("Error")
-        .setDescription("You must be in a server to use this command!")
-        .setColor(0xaf3838)
-        .setThumbnail(
-          "https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png",
-        )
-        .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" });
-      return json({
-        type: 4,
-        data: {
-          embeds: [embed],
-        },
-      });
+          .setTitle("Error")
+          .setDescription("You must be in a server to use this command!")
+          .setColor(0xaf3838)
+          .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+          .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" });
+      return json({ type: 4, data: { embeds: [embed] } });
     }
     const userId = interaction.member.user.id;
     const username = interaction.member.user.username;
@@ -84,85 +79,49 @@ export const subscribe: Command = {
     const param = data.options?.find((opt) => opt.name === "id");
     if (!param) {
       const embed = new EmbedBuilder()
-        .setTitle("Missing Parameters")
-        .setDescription("You must provide parameters")
-        .setColor(0xaf3838)
-        .setThumbnail(
-          "https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png",
-        )
-        .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
-        .setTimestamp(new Date());
-      return json({
-        type: 4,
-        data: {
-          embeds: [embed],
-        },
-      });
+          .setTitle("Missing Parameters")
+          .setDescription("You must provide parameters")
+          .setColor(0xaf3838)
+          .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+          .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
+          .setTimestamp(new Date());
+      return json({ type: 4, data: { embeds: [embed] } });
     }
     if (param.type !== 3) {
       const embed = new EmbedBuilder()
-        .setTitle("Invalid Parameters")
-        .setDescription("Invalid type of parameters")
-        .setColor(0xaf3838)
-        .setThumbnail(
-          "https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png",
-        )
-        .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
-        .setTimestamp(new Date());
-      return json({
-        type: 4,
-        data: {
-          embeds: [embed],
-        },
-      });
+          .setTitle("Invalid Parameters")
+          .setDescription("Invalid type of parameters")
+          .setColor(0xaf3838)
+          .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+          .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
+          .setTimestamp(new Date());
+      return json({ type: 4, data: { embeds: [embed] } });
     }
-
     kv.set(["user", userId], { username, id: userId, globalName });
-
-    // Check if bridge node ID exists
-    const nodesIds = await bridgeNodesAPI.getAllBridgeNodesIds();
+    const nodesIds = await nodesAPI.getAllNodesIds();
     if (!nodesIds.includes(param.value)) {
       const embed = new EmbedBuilder()
-        .setTitle("Invalid Node Bridge Id")
-        .setDescription(
-          "Please check that your bridge id is correct and try again",
-        )
-        .setColor(0xaf3838)
-        .setThumbnail(
-          "https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png",
-        )
-        .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
-        .setTimestamp(new Date());
-
-      return json({
-        type: 4,
-        data: {
-          embeds: [embed],
-        },
-      });
+          .setTitle("Invalid Node Id")
+          .setDescription("Please check that your node id is correct and try again")
+          .setColor(0xaf3838)
+          .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+          .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
+          .setTimestamp(new Date());
+      return json({ type: 4, data: { embeds: [embed] } });
     }
-
+    const nodeType = await nodesAPI.getNodeType(param.value);
     kv.set(["subscription", userId, param.value], {
       userId,
-      nodeBridgeId: param.value,
+      nodeId: param.value,
       subscribedAt,
     });
-
     const embed = new EmbedBuilder()
-      .setTitle("Subscription Success")
-      .setDescription(`You have been subscribed to **\`${param.value}\`**`)
-      .setColor(0x7b2bf9)
-      .setThumbnail(
-        "https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png",
-      )
-      .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
-      .setTimestamp(new Date());
-
-    return json({
-      type: 4,
-      data: {
-        embeds: [embed],
-      },
-    });
+        .setTitle("Subscription Success")
+        .setDescription(`You have been subscribed to **\`${nodeType ?? "Unknown"}\`** node **\`${param.value}\`**`)
+        .setColor(0x7b2bf9)
+        .setThumbnail("https://raw.githubusercontent.com/DTEAMTECH/contributions/refs/heads/main/celestia/utils/bridge_metrics_checker.png")
+        .setFooter({ text: "Powered by www.dteam.tech \uD83D\uDFE0" })
+        .setTimestamp(new Date());
+    return json({ type: 4, data: { embeds: [embed] } });
   },
 };
