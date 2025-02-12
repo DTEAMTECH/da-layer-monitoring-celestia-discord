617c23d008d823aa3a26ae372c2691e8421a4d80
Eugene <doodleoocrypto@gmail.com>
2025-02-12 18:19:30 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -12,31 +12,30 @@ type RetryOptions = {
 };
 
 function withRetry<Args extends unknown[], Result>(
-  fn: (...args: Args) => Promise<Result>,
-  options?: RetryOptions,
+    fn: (...args: Args) => Promise<Result>,
+    options?: RetryOptions,
 ): (...args: Args) => Promise<Result> {
   return async function (...args: Args): Promise<Result> {
     return await pRetry(
-      async () => {
-        try {
-          return await fn(...args); // Call the original function
-        } catch (error) {
-          console.warn(`Retry failed: ${(error as Error).message}`);
-          throw error; // Retry on error
-        }
-      },
-      {
-        retries: options?.retries ?? 5,
-        factor: options?.factor ?? 2,
-        minTimeout: options?.minTimeout ?? 1000,
-        maxTimeout: options?.maxTimeout ?? 5000,
-        randomize: options?.randomize ?? true,
-      },
+        async () => {
+          try {
+            return await fn(...args);
+          } catch (error) {
+            console.warn(`Retry failed: ${(error as Error).message}`);
+            throw error;
+          }
+        },
+        {
+          retries: options?.retries ?? 5,
+          factor: options?.factor ?? 2,
+          minTimeout: options?.minTimeout ?? 1000,
+          maxTimeout: options?.maxTimeout ?? 5000,
+          randomize: options?.randomize ?? true,
+        },
     );
   };
 }
 
-// todo: move to services
 export class DiscordApiHelper {
   public rest: REST;
   public config: {
@@ -45,8 +44,8 @@ export class DiscordApiHelper {
     guildId: string;
   };
   sendMessage: (
-    channelId: string,
-    option: { message?: string; embed?: EmbedBuilder },
+      channelId: string,
+      option: { message?: string; embed?: EmbedBuilder },
   ) => Promise<void>;
 
   constructor(config: { token: string; clientId: string; guildId: string }) {
@@ -56,13 +55,12 @@ export class DiscordApiHelper {
   }
 
   async sendMessageRaw(
-    channelId: string,
-    { message, embed }: { message?: string; embed?: EmbedBuilder },
+      channelId: string,
+      { message, embed }: { message?: string; embed?: EmbedBuilder },
   ) {
     if (!message && !embed) {
       throw new Error("Either message or embed must be provided.");
     }
-
     const mainMessage = message ? { content: message } : { embeds: [embed] };
     await this.rest.post(Routes.channelMessages(channelId), {
       body: mainMessage,
@@ -84,6 +82,27 @@ export class DiscordApiHelper {
   sendEmbedMessageBotChannel(embed: EmbedBuilder) {
     return this.sendEmbedMessage(config.BOT_CHANNEL_ID, embed);
   }
+
+  async createDMChannel(userId: string): Promise<string> {
+    try {
+      const dmChannel = await this.rest.post(Routes.userChannels(), {
+        body: { recipient_id: userId },
+      }) as { id: string };
+      return dmChannel.id;
+    } catch (error) {
+      console.error("Error creating DM channel for user:", userId, error);
+      throw error;
+    }
+  }
+
+  async sendEmbedMessageUser(userId: string, embed: EmbedBuilder) {
+    try {
+      const dmChannelId = await this.createDMChannel(userId);
+      await this.sendEmbedMessage(dmChannelId, embed);
+    } catch (error) {
+      console.error("Error sending embed message to user:", userId, error);
+    }
+  }
 }
 
 export const disApi = new DiscordApiHelper({
@@ -93,18 +112,16 @@ export const disApi = new DiscordApiHelper({
 });
 
 export const isObject = (value: unknown) =>
-  typeof value === "object" && value !== null;
+    typeof value === "object" && value !== null;
 
 export function isRecent(
-  referenceTimeIso: string | null,
-  thresholdMinutes: number = TIME_RECENT_ALERT_IN_MINUTES,
+    referenceTimeIso: string | null,
+    thresholdMinutes: number = TIME_RECENT_ALERT_IN_MINUTES,
 ): boolean {
   if (!referenceTimeIso) return false;
-
   const referenceTime = new Date(referenceTimeIso);
   const targetTime = new Date();
   const diffMilliseconds = targetTime.getTime() - referenceTime.getTime();
   const diffMinutes = Math.floor(diffMilliseconds / 60000);
-
   return diffMinutes < thresholdMinutes;
 }
